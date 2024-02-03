4b730bf90b84459d25a4e31bf131d0ccb638cf3e
IrinaBelenko <118209690+IrinaBelenko@users.noreply.github.com>
2024-02-04 00:05:05 +0200

    hero json file open

--- app/src/main/java/com/example/homework16/ApiInterface.kt
+++
@@ -1,9 +0,0 @@
-package com.example.homework16
-
-import io.reactivex.Single
-import retrofit2.http.GET
-
-interface ApiInterface {
-    @GET("/superhero-api/api/all.json")
-    fun getHeroes(): Single<SuperHeroResponse>
-}
--- app/src/main/java/com/example/homework16/MainActivity.kt
+++ app/src/main/java/com/example/homework16/MainActivity.kt
@@ -3,29 +3,30 @@ package com.example.homework16
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
+import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
-import io.reactivex.android.schedulers.AndroidSchedulers
-import io.reactivex.schedulers.Schedulers
+import com.google.gson.Gson
+import java.io.BufferedReader
+import java.io.InputStreamReader
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         val recyclerView: RecyclerView = findViewById(R.id.recyclerView)
-        val api = ApiClient.client.create(ApiInterface::class.java)
-        api.getHeroes().subscribeOn(Schedulers.io())
-            .observeOn(AndroidSchedulers.mainThread())
-            .subscribe({
-                println(it)
-
-            }, {
-
-                Toast.makeText(this, "Fetch error ${it.message}", Toast.LENGTH_LONG).show() })
+        val jsonFile = assets.open("all.json")
+        val bufferedReader = BufferedReader(InputStreamReader(jsonFile))
+        val stringBuilder = StringBuilder()
+        bufferedReader.useLines {
+            lines -> lines.forEach { stringBuilder.append(it) }
+        }
+        val jsonString = stringBuilder.toString()
+        val data = Gson().fromJson(jsonString, Heroes::class.java)
+        val items = data
+        val myAdapter = RecyclerViewAdapter(items as MutableList<Hero>) {
+            Toast.makeText(this, "${it} clicked", Toast.LENGTH_SHORT).show()
+        }
+        recyclerView.adapter = myAdapter
+        recyclerView.layoutManager = LinearLayoutManager(this)
     }
 }
-
-data class SuperHeroResponse(val heroes: Heroes)
-data class Heroes(val hero: List<Hero>)
-data class Hero(val name: String, val id: String, val slug: String)
-//data class Appear(val gender: String, val race: String)
-//data class HeroImages(val xs: String, val sm: String)
